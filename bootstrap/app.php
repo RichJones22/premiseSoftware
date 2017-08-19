beffb14742326294866202068bda536ccd5faf85
Rich Jones <jones_rich@yahoo.com>
2017-08-19 09:48:32 -0500

    =added CheckForLocalLogging() back

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -44,22 +44,19 @@ $app->singleton(
 );
 
 // to log to your local environment, set APP_LOCAL_LOG=true in your .env file.
-
-// commented out for first deploy.
-//
-//$environment = new CheckForLocalLogging();
-//if( ! $environment->isLogLocalSet())
-//{
-//    if ($environment->getEnvironment() === 'production') {
-//        $app->configureMonologUsing(function(Monolog\Logger $monoLog){
-//            $monoLog->pushHandler(new Monolog\Handler\SyslogHandler('papertrail'));
-//        });
-//    } else {
-//        $app->configureMonologUsing(function(Monolog\Logger $monoLog){
-//            $monoLog->pushHandler(new Monolog\Handler\SyslogUdpHandler('logs5.papertrailapp.com', 51932));
-//        });
-//    }
-//}
+$environment = new CheckForLocalLogging();
+if( ! $environment->isLogLocalSet())
+{
+    if ($environment->getEnvironment() === 'production') {
+        $app->configureMonologUsing(function(Monolog\Logger $monoLog){
+            $monoLog->pushHandler(new Monolog\Handler\SyslogHandler('papertrail'));
+        });
+    } else {
+        $app->configureMonologUsing(function(Monolog\Logger $monoLog){
+            $monoLog->pushHandler(new Monolog\Handler\SyslogUdpHandler('logs5.papertrailapp.com', 51932));
+        });
+    }
+}
 
 /*
 |--------------------------------------------------------------------------
