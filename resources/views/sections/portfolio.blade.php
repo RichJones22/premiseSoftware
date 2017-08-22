80d634db09a2659813c521e1090c5ee85e16f1da
Rich Jones <jones_rich@yahoo.com>
2017-08-21 19:42:42 -0500

    add https to youtube vid

--- resources/views/sections/portfolio.blade.php
+++ resources/views/sections/portfolio.blade.php
@@ -166,7 +166,7 @@
                         <br/>
                         <div class="embed-responsive embed-responsive-16by9">
                             <iframe id="timetraxVid"
-                                    src="http://www.youtube.com/embed/TsbYrsVdWEI?modestbranding=1&showinfo=0&rel=0"
+                                    src="https://www.youtube.com/embed/TsbYrsVdWEI?modestbranding=1&showinfo=0&rel=0"
                                     allowfullscreen>
                             </iframe>
                         </div>
