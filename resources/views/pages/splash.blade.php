4ddabee1d9b83e8843da2b02eb4251e8b2269bc8
Rich Jones <jones_rich@yahoo.com>
2017-08-26 22:36:12 -0500

    need to move where '#app' was placed; caused mixitup.js not to work

--- resources/views/pages/splash.blade.php
+++ resources/views/pages/splash.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
 
+    @include('sections.banner');
+
+    @include('sections.about')
+
+    @include('sections.poles-first')
+
+    @include('sections.portfolio')
+
+    @include('sections.poles-second')
+
     <div id="app">
-        @include('sections.banner');
-
-        @include('sections.about')
-
-        @include('sections.poles-first')
-
-        @include('sections.portfolio')
-
-        @include('sections.poles-second')
-
         @include('sections.contact')
 
         @include('sections.footer')
